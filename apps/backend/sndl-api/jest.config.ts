97b1183d0743f547615af6a726ace289264b56e7
Viktor Ivanov <vitochek337@gmail.com>
2024-02-27 18:21:51 +0200

    Created base projects

---
+++ apps/backend/sndl-api/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'sndl-api',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/backend/sndl-api',
+};
