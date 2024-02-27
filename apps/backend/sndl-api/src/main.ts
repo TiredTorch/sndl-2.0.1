815604344053fac56fa2a34b2650afbb27624901
Viktor Ivanov <vitochek337@gmail.com>
2024-02-27 19:06:05 +0200

    Configurated base resourses

--- apps/backend/sndl-api/src/main.ts
+++ apps/backend/sndl-api/src/main.ts
@@ -1,21 +1,47 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-import { AppModule } from './app/app.module';
+import { SndlApiLibModule } from '@sndl-api-lib';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(SndlApiLibModule);
+
   const globalPrefix = 'api';
+  const globalDocsPrefix = 'docs';
+
   app.setGlobalPrefix(globalPrefix);
+
+  const swaggerConfig = new DocumentBuilder()
+			.setTitle("Sndl api")
+			.setDescription("Soundle REST API documentation")
+			.setVersion("1.0")
+			// .addBearerAuth(
+			// 	{
+			// 		type: "http",
+			// 		scheme: "bearer",
+			// 		bearerFormat: "JWT",
+			// 		in: "header"
+			// 	},
+			// 	"Auth"
+			// )
+			.build();
+
+  SwaggerModule.setup(
+    globalDocsPrefix,
+    app,
+    SwaggerModule.createDocument(
+      app,
+      swaggerConfig
+    )
+  );
+
   const port = process.env.PORT || 3000;
+
   await app.listen(port);
+
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}/${globalDocsPrefix}`
   );
 }
 
--- libs/sndl-api-lib/src/lib/sndl-api-lib.module.ts
+++ libs/sndl-api-lib/src/lib/sndl-api-lib.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { ChatModule } from './chat/chat.module';
+import { PostModule } from './post/post.module';
+import { SongModule } from './song/song.module';
+import { UserModule } from './user/user.module';
 
 @Module({
+  imports: [
+    AuthModule,
+    ChatModule,
+    PostModule,
+    SongModule,
+    UserModule
+  ],
   controllers: [],
   providers: [],
   exports: [],
